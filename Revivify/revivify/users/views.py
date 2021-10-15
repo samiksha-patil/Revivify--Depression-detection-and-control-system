77d4c2a65a3a48cd611fdca19eca999c0fef9292
sakshipatil16 <sakshi16.patil2000@gmail.com>
2021-10-15 20:02:23 +0530

    Login added

---
+++ Revivify/revivify/users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+# Create your views here.
+from .forms import UserRegisterForm
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'{ username }, Your account has been created! You are now able to login')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request,'users/register.html',{'form': form})
+
+def home(request):
+     
+     return render(request,'home.html')
