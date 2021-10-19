6e1ff7f3f4b4f9a86521ce41e6206c21c70834d8
sakshipatil16 <sakshi16.patil2000@gmail.com>
2021-10-19 15:37:33 +0530

    added form

--- Revivify/revivify/revivify/urls.py
+++ Revivify/revivify/revivify/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html') ,name='logout'),
     path('phq',tracker_view.phq_form, name='phq_form'),
+    path('dass21',tracker_view.dass21_form, name='dass21_form'),
 ]
 
 
